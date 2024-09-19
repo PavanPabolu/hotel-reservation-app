c66c95ec20fb07b4f0718bd434a2fd8196503376
PavanPabolu <PavanPabolu.edu@gmail.com>
2024-09-19 23:22:04 +0530

    Form validation using formbuilder

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit{
+ 
+  reservationForm : FormGroup = new FormGroup({});
 
+  //for dependancy injection for formbuilder object when an instance created for  this componenet class.
+  constructor(private formBuilder : FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+        checkInDate : ['', Validators.required],
+        checkOutDate : ['', Validators.required],
+        guestName : ['', Validators.required],
+        guestEmail : ['', [Validators.required, Validators.email]],
+        roomNumber : ['', Validators.required]
+    });
+  }
+
+
+  onSubmit() : void {
+    console.log(1);
+    if(this.reservationForm.valid){
+      console.log("valid.!");
+    }
+
+  }
 }
